3ce410ec5731cc805dfa9a94e57829a649ba5f6f
thomas <tgeffroy@gmail.com>
2025-07-02 21:26:13 +0200

    Base

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Accueil Smartbike</title>
+</head>
+
+<body class="body">
+    
+    <div class="logopage">
+        <img id="img" src="logo.png">
+    </div>
+</body>
+</html>
